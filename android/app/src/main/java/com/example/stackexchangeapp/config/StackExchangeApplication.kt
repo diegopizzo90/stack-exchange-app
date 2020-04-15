1820baf763cb2fc6280547d3cc9c33950fa6635b
Diego Pizzo <>
2020-04-15 15:20:00 +0100

    [main-screen] create application class and add Koin configuration

---
+++ android/app/src/main/java/com/example/stackexchangeapp/config/StackExchangeApplication.kt
@@ -0,0 +1,17 @@
+package com.example.stackexchangeapp.config
+
+import android.app.Application
+import com.example.stackexchangeapp.business.network.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class StackExchangeApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@StackExchangeApplication)
+            modules(listOf(networkModule))
+        }
+    }
+}
