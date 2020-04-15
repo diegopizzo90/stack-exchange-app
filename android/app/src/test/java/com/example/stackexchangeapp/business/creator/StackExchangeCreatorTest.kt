0940c38b5713c6d8ac5d28be149898f94fac784f
Diego Pizzo <>
2020-04-15 15:22:26 +0100

    [main-screen] add creator unit test class

---
+++ android/app/src/test/java/com/example/stackexchangeapp/business/creator/StackExchangeCreatorTest.kt
@@ -0,0 +1,33 @@
+package com.example.stackexchangeapp.business.creator
+
+import com.example.stackexchangeapp.business.dataviewmodel.UserView
+import com.example.stackexchangeapp.business.network.model.User
+import com.example.stackexchangeapp.business.network.model.UsersResponse
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class StackExchangeCreatorTest {
+
+    @Test
+    fun fromModelToDataViewModel_() {
+        val result = StackExchangeCreator.fromModelToDataViewModel(responseModel)
+        assertEquals(result, dataViewModel)
+    }
+
+    companion object {
+        private val responseModel = UsersResponse(
+            listOf(
+                User(12, "username1"),
+                User(22, "username2")
+            )
+        )
+
+        private val dataViewModel = listOf(
+            UserView("12", "username1"),
+            UserView("22", "username2")
+        )
+    }
+}
